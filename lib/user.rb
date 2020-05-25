1b74c203a217ce8930cef56e5570ce798512caa6
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-25 01:09:49 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,10 @@
-class User 
+class User
   attr_accessor :first_name, :last_name
 
   KNOWLEDGE = []
 
-  def teach 
-    KNOWLEDGE.sample 
-  end 
+  def teach
+    KNOWLEDGE.sample
+  end
 end
     
